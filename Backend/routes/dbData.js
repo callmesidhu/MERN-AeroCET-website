061e2284076eaea1409fd64d34699cd3879e836b
S SIDHARTH <sidharthpunalur@gmail.com>
2024-11-06 18:51:31 -0800

    static db data download added

--- Backend/routes/dbData.js
+++ Backend/routes/dbData.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const router = express.Router();    
-        
+const path = require('path'); 
+const router = express.Router();
+
 router.get('/data', (req, res) => {
-        res.send('db data');
-      });
+    res.download(path.join(__dirname, '.././public/data/aerocet.pdf'));
+});
 
 module.exports = router;
